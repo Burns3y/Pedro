4e4f19029bb2278c16ad4bffea9a7d727fdeac0a
Burmsey <joehurndell1@gmail.com>
2024-05-16 11:09:31 +1200

    Start screen COMPLETE

--- scenes/levels/Start Screen/start_screen.gd
+++ scenes/levels/Start Screen/start_screen.gd
@@ -1,9 +1,18 @@
 extends Node2D
 
+var SIGNAL = "false"
+
 func _on_start_button_pressed():
-	print("pressed started")
+	SIGNAL = "true"
+	
 
 func _on_quit_button_pressed():
 	get_tree().quit()
 	
 
+var SPEED = 0.2
+
+func _physics_process(delta):
+	if SIGNAL == "true":
+		position.y += SPEED
+		SPEED = -1000
