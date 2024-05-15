5407b473ec9556e030e7e74e27596015b86d7a83
Burmsey <joehurndell1@gmail.com>
2024-05-15 14:47:26 +1200

    idk but i did some work and it needs pushed lol. I assume its just the start screen

--- scenes/levels/start_screen.gd
+++ scenes/levels/start_screen.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
+var button_initial_position = 100
 
+
+func _ready():
+	# Store the initial position of the button
+	button_initial_position = $Button.rect_positio
 
 func _on_start_button_pressed():
-	#$start_button.rect_position.y = 300
+	$start_button.rect_min_size.y += 100
 	print("pressed started")
+	button_initial_position.y += 100
+	$Button.rect_position = button_initial_position
 
 
 func _on_quit_button_pressed():
