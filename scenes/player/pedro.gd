cdaa2af17ca5a9e66499791a50b1bf16cde1a60a
Burmsey <joehurndell1@gmail.com>
2024-05-23 11:29:18 +1200

    fixed being able to double jump on boxes/grass and dirt

--- scenes/player/pedro.gd
+++ scenes/player/pedro.gd
@@ -18,14 +18,6 @@ func _physics_process(delta):
 		if not is_on_floor():
 			velocity.y += gravity * delta + 10
 
-		# Handle jump.
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			
-		# Wall jump
-		elif Input.is_action_just_pressed("jump") and is_on_wall_only() and velocity.y >= -200 and position.x >= 5500:
-			velocity.y = JUMP_VELOCITY 
-
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("left", "right")
@@ -33,6 +25,17 @@ func _physics_process(delta):
 			velocity.x = direction * SPEED * delta
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
+			
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			
+		# Wall jump
+		elif Input.is_action_just_pressed("jump") and is_on_wall_only() and velocity.y >= -200 and position.x >= 5500:
+			velocity.y = JUMP_VELOCITY 
+		elif Input.is_action_just_pressed("jump") and is_on_wall_only() and position.x <= 5500:
+			velocity.x = 0
+			velocity.y = 0
 
 		move_and_slide()
 
