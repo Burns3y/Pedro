f7a64965f98a5b0ed0084db6c496ed76ad21caa7
Burmsey <joehurndell1@gmail.com>
2024-05-23 11:47:34 +1200

    kinda fixed wall jump (now player doesnt move)

--- scenes/player/pedro.gd
+++ scenes/player/pedro.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 var SPEED = 13000
 const JUMP_VELOCITY = -600
 var started: bool = false
+var NO_WALL_JUMP = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -21,10 +22,13 @@ func _physics_process(delta):
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var direction = Input.get_axis("left", "right")
-		if direction:
-			velocity.x = direction * SPEED * delta
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		if NO_WALL_JUMP == true:
+			await get_tree().create_timer(0.5).timeout
+		elif NO_WALL_JUMP == false:
+			if direction:
+				velocity.x = direction * SPEED * delta
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
 			
 		# Handle jump.
 		if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -34,8 +38,9 @@ func _physics_process(delta):
 		elif Input.is_action_just_pressed("jump") and is_on_wall_only() and velocity.y >= -200 and position.x >= 5500:
 			velocity.y = JUMP_VELOCITY 
 		elif Input.is_action_just_pressed("jump") and is_on_wall_only() and position.x <= 5500:
-			velocity.y += gravity * delta + 1000
+			velocity.y += gravity * delta + 500
 			velocity.x = -100
+			NO_WALL_JUMP = true
 
 		move_and_slide()
 
