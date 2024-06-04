da70dab14f6820e42f4017e890df2a88d2d0a2f6
Burns3y <21oburns@wakatipu.school.nz>
2024-06-05 10:09:24 +1200

    Made timer to the nearest 5ms

--- scenes/UI/stats.gd
+++ scenes/UI/stats.gd
@@ -1,25 +1,38 @@
 extends CanvasLayer
 
-var time_spent: int = 0
+var ms: int = 0
+var s: int = 0
+var m: int = 0
 var ended: bool = false
 
 func _process(_delta):
+	
+	
+	if ms > 99:
+		s += 1
+		ms = 0
+	if s > 60:
+		m += 1
+		s = 0
 
 	if ($Timer.is_stopped() and $"../Start_Screen".SIGNAL) or $"../../Pedro".ended:
 		#if paused:
 			#print("Paused")
 		#elif not paused:
 			#print("Not paused")
+			
 		$Timer.start()
 
 func _on_timer_timeout():
 	if $"../../Pedro".started == true:
-		time_spent += 1
-		var timer = "Timer: " + str(time_spent)
+		ms += 5
+		var timer = "Timer: " + str(m) + " : " + str(s) + " : " + str(ms)
 		$Label.text = timer
 
 
 func _on_start_screen_started():
-	time_spent = 0
-	$Label.text = "Timer: 0"
+	ms = 0
+	s = 0
+	m = 0
+	$Label.text = "Timer: 0 : 0 : 0"
 
