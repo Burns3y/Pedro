da4991391c98588c1d6e0b8220766f3b54d32a04
Burns3y <21oburns@wakatipu.school.nz>
2024-06-06 23:16:40 +1200

    Taco bobs up and down

---
+++ scenes/items/taco.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var direction: Vector2 = Vector2.DOWN
+var speed = 20
+var initial_y
+var current_direction: int = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	initial_y = position.y
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+		
+	if (position.y > initial_y + 10) or (position.y < initial_y -10):
+		current_direction *= -1
+	
+	if (position.y > initial_y + 9) or (position.y < initial_y -9):
+		speed = 10
+	else:
+		speed = 20
+	position += speed * direction * current_direction * delta
