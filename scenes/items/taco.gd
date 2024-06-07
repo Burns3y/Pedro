14f9a044ed4664ae66bc6948e0f1319611faa44a
Burns3y <21oburns@wakatipu.school.nz>
2024-06-07 14:01:16 +1200

    Taco disappears when player touches it

--- scenes/items/taco.gd
+++ scenes/items/taco.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
 var direction: Vector2 = Vector2.DOWN
-var speed = 20
-var initial_y
+var speed:int = 20
+var initial_y: int
 var current_direction: int = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -10,7 +10,6 @@ func _ready():
 	initial_y = position.y
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
 		
 	if (position.y > initial_y + 10) or (position.y < initial_y -10):
 		current_direction *= -1
@@ -20,3 +19,10 @@ func _process(delta):
 	else:
 		speed = 20
 	position += speed * direction * current_direction * delta
+
+
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		queue_free()
