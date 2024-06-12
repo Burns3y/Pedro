9597abae59dc20054bbd27edbf9a109796ba3b31
Burmsey <joehurndell1@gmail.com>
2024-06-13 10:27:17 +1200

    polease idaddy character nad backgroud stugfgf

--- scenes/UI/stats.gd
+++ scenes/UI/stats.gd
@@ -3,8 +3,6 @@ extends CanvasLayer
 var ms: int = 0
 var s: int = 0
 var m: int = 0
-var timer_active = false
-var ended: bool = false
 var score: int = 0
 
 func _process(delta):
@@ -15,25 +13,9 @@ func _process(delta):
 		m += 1
 		s = 0
 
-	if ($Timer.is_stopped() and $"../Start_Screen".SIGNAL) or $"../../Pedro".ended:
-		if not $"../..".paused:
-			$Timer.start()
-			timer_active = true
-			
-	if not $"../..".paused and $"../Start_Screen".SIGNAL:
-		if timer_active:
-			ms += 100 * delta
+		ms += 100 * delta
 		var timer = "Timer: " + str(m) + " : " + str(s) + " : " + str(ms)
 		$TimerText.text = timer
-
-
-#func _on_timer_timeout():
-	####This does nothing rn it doesn't work
-	#if $"../../Pedro".started == true:
-		#print("Started 2")
-		#ms += 5
-		#var timer = "Timer: " + str(m) + " : " + str(s) + " : " + str(ms)
-		#$TimerText.text = timer
 
 
 func _on_start_screen_started():
