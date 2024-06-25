fbf5378c58c8e7d5d4e4bf8ac6a38a015e61b5ba
Burns3y <21oburns@wakatipu.school.nz>
2024-06-26 10:24:23 +1200

    torches now flicker more complicatedly

--- scenes/items/torch.gd
+++ scenes/items/torch.gd
@@ -1,6 +1,15 @@
 extends Sprite2D
 
-var fire_rgbs = [Color(0.976, 0.863, 0.478), Color(0.839, 0.518, 0.18), Color(0.765, 0.596, 0.031), Color(0.953, 0.745, 0.043), Color(0.965, 0.796, 0.231), Color(0.973, 0.847, 0.424), Color(0.98, 0.898, 0.616)]
+var fire_rgbs = [
+Color(0.976, 0.863, 0.478), 
+Color(0.839, 0.518, 0.18), 
+Color(0.765, 0.596, 0.031), 
+Color(0.953, 0.745, 0.043), 
+Color(0.965, 0.796, 0.231), 
+Color(0.973, 0.847, 0.424), 
+Color(0.98, 0.898, 0.616)
+]
+
 var new_flicker_colour
 var initial_colour
 # Called when the node enters the scene tree for the first time.
@@ -9,11 +18,11 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	new_flicker_colour = fire_rgbs[randi() % fire_rgbs.size()]
 	#$PointLight2D.color = flicker_colour
-	var tween = get_tree().create_tween()
-	tween.interpolate_value(initial_colour, new_flicker_colour,0 , 0, Tween.TRANS_LINEAR, Tween.EASE_IN)
+	Tween.interpolate_value(initial_colour, new_flicker_colour,5 , 16, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	initial_colour = new_flicker_colour
+	$PointLight2D.color = new_flicker_colour
 	print("INTERPOLATED VALUE AAAAAA\nInitial colour:", initial_colour, "\nNew flicker colour:", new_flicker_colour)
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(10.0).timeout
