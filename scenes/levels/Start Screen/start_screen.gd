005367e92e3b9166d3c2f894a2cf500daf0b10fe
Burns3y <21oburns@wakatipu.school.nz>
2024-07-22 12:34:43 +1200

    Revert "Level 1 button works"
    This reverts commit 95e0d4674993cdf291629b3f7d52afb2a921c8fb.

--- scenes/levels/Start Screen/start_screen.gd
+++ scenes/levels/Start Screen/start_screen.gd
@@ -1,12 +1,10 @@
 extends CanvasLayer
-#SIGNAL checks if the game has started
 var SIGNAL: bool = false
 
 signal started
 
 #Removes all enemies and tacos, disables the buttons, unpauses the game
 func starting_game():
-	print($"../..".current_level)
 	$"../..".remove_enemies()
 	$"../..".remove_tacos()
 	SIGNAL = true
@@ -17,7 +15,7 @@ func starting_game():
 	
 	#Emits started() signal
 	started.emit()
-	
+
 func _on_start_button_pressed():
 	starting_game()
 
@@ -25,28 +23,26 @@ func _on_tutorial_button_pressed():
 	$"../../Pedro".ended = false
 	$"../..".current_level = 0
 	starting_game()
+	
+	
+func _on_quit_button_pressed():
+	get_tree().quit()
 
 
 func _on_level_1_button_pressed():
 	$"../../Pedro".ended = false
 	$"../..".current_level = 1
-	call_deferred("starting_game")
-
-
-func _on_quit_button_pressed():
-	get_tree().quit()
+	starting_game()
 
 
 func _process(_delta):
 	if $"../../Pedro".ended:
-		print("Ended is true")
 		SIGNAL = false
 		$Panel.modulate.a = 1
 		if $"../..".current_level < 2:
 			$Panel/start_button.text = "Next Level"
 
 	elif SIGNAL == true and not $"../..".game_is_paused:
-		print("started and not paused")
 		
 		while $Panel.modulate.a > 0:
 			$Panel.modulate.a -= 0.05
@@ -54,13 +50,11 @@ func _process(_delta):
 		$Panel/start_button.text = "Start Game"
 
 	elif $Panel.modulate.a < 1:
-		print("modulated to 1")
 
 		$Panel.modulate.a = 1
 
 	if $"../..".game_is_paused and not $"../../Pedro".ended:
 		$Panel.modulate.a = 1
-		print("paused")
 
 		for i in $Panel.get_children():
 			if i is Button:
